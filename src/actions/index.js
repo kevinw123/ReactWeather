a388c3dd2d4f3ccd430753ffb3e28750bd2b4979
Kevin Wong <kevinwong_895@hotmail.com>
2017-06-03 10:59:22 -0400

    Adding sparklines for temperature

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '47fee4864d1fb034ed538a73c730c553';
-const ROOT_URL= `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL= `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
